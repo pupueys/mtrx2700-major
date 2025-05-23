0779a8867263a5c12150025f2fe3fd0ef0455199
Nadeen005 <nper0081@uni.sydney.edu.au>
2025-05-23 14:42:32 +1000

    Update game_logic.c

--- WireCut/Final_project/Src/game_logic.c
+++ WireCut/Final_project/Src/game_logic.c
@@ -5,37 +5,49 @@
 #include "game_logic.h"
 
 #define CORRECT_WIRE_COUNT 2
+#define WRONG_WIRE_COUNT 2
 
-static volatile uint8_t wire_pulled[4] = {0, 0, 0, 0};         // Keep track of the wires pulled
-static const uint8_t correct_wires[4] = {1, 0, 1, 0};         // hard code wires 1 and 3 to be correct
-static volatile uint8_t correct_count = 0;                    // Number of correct wires pulled
-static volatile uint8_t game_over = 0;                        // Indicate game over
+static volatile uint8_t wire_pulled[6] = {0, 0, 0, 0, 0, 0};        // Keep track of the wires pulled
+static const uint8_t correct_wires[6] = {1, 0, 1, 0, 0, 0};         // hard code wires 1 and 3 to be correct
+static volatile uint8_t correct_count = 0;                          // Number of correct wires pulled
+static volatile uint8_t wrong_count = 0;                            // Number of wrong wires pulled
+static volatile uint8_t game_over = 0;                              // Indicate game over
 
 
-// Enable interrupts on falling edge for PC1–PC4
+// Enable interrupts on falling edge for PE4–PE9
 void enable_wire_interrupts() {
 	__disable_irq();  // Disable global interrupts while configuring
 
 	// 1. Enable SYSCFG clock for EXTI configuration
 	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
 
-	// 2. Map EXTI lines 4–7 to Port E using SPL-style macros
+	// 2. Map EXTI lines 4–7 to Port E using EXTICR2 (EXTI4–7)
 	SYSCFG->EXTICR[1] = SYSCFG_EXTICR2_EXTI4_PE |
 	                    SYSCFG_EXTICR2_EXTI5_PE |
 	                    SYSCFG_EXTICR2_EXTI6_PE |
 	                    SYSCFG_EXTICR2_EXTI7_PE;
 
-	// 3. Select falling edge trigger to detect wire being pulled (goes from 3.3V to 0V)
-	EXTI->FTSR |= EXTI_FTSR_TR4 | EXTI_FTSR_TR5 | EXTI_FTSR_TR6 | EXTI_FTSR_TR7;
+	//    Map EXTI lines 8–9 to Port E using EXTICR3 (EXTI8–11)
+	SYSCFG->EXTICR[2] &= ~(
+	    SYSCFG_EXTICR3_EXTI8 | SYSCFG_EXTICR3_EXTI9);  // Clear bits first
+	SYSCFG->EXTICR[2] |= SYSCFG_EXTICR3_EXTI8_PE |
+	                     SYSCFG_EXTICR3_EXTI9_PE;
 
-	// 4. Unmask (enable) EXTI lines 4–7
-	EXTI->IMR |= EXTI_IMR_MR4 | EXTI_IMR_MR5 | EXTI_IMR_MR6 | EXTI_IMR_MR7;
+	// 3. Select falling edge trigger for EXTI4–9
+	EXTI->FTSR |= EXTI_FTSR_TR4 | EXTI_FTSR_TR5 |
+	              EXTI_FTSR_TR6 | EXTI_FTSR_TR7 |
+	              EXTI_FTSR_TR8 | EXTI_FTSR_TR9;
 
-	// 5. Enable NVIC IRQs for EXTI lines
+	// 4. Unmask (enable) EXTI4–9
+	EXTI->IMR |= EXTI_IMR_MR4 | EXTI_IMR_MR5 |
+	             EXTI_IMR_MR6 | EXTI_IMR_MR7 |
+	             EXTI_IMR_MR8 | EXTI_IMR_MR9;
+
+	// 5. Enable NVIC IRQs
 	NVIC_SetPriority(EXTI4_IRQn, 1);       // PE4
 	NVIC_EnableIRQ(EXTI4_IRQn);
 
-	NVIC_SetPriority(EXTI9_5_IRQn, 1);     // PE5–PE7
+	NVIC_SetPriority(EXTI9_5_IRQn, 1);     // PE5–PE9
 	NVIC_EnableIRQ(EXTI9_5_IRQn);
 
 	__enable_irq();  // Re-enable global interrupts
@@ -63,6 +75,15 @@ void EXTI9_5_IRQHandler(void) {
 		EXTI->PR |= EXTI_PR_PR7;
 		handle_wire_pull(3);           // Wire 4 = PE7
 	}
+	if (EXTI->PR & EXTI_PR_PR8) {
+		EXTI->PR |= EXTI_PR_PR8;
+		handle_wire_pull(4);           // Wire 3 = PE6
+
+	}
+	if (EXTI->PR & EXTI_PR_PR9) {
+		EXTI->PR |= EXTI_PR_PR9;
+		handle_wire_pull(5);           // Wire 4 = PE7
+	}
 }
 
 
@@ -75,16 +96,18 @@ void handle_wire_pull(int wire_index) {
 
     if (correct_wires[wire_index]) {
         correct_count++;
+
     }
     else {
-        reduce_time(15); // Reduce time by 15 seconds
+        reduce_time(10); // Reduce time by 10 seconds
         update_display(time_left);
+        wrong_count++;
     }
 
     if (correct_count == CORRECT_WIRE_COUNT) {
         end_game(1); // Game over player wins
     }
-    else if (time_left == 0) {
+    else if (time_left == 0 || wrong_count ==  WRONG_WIRE_COUNT) {
         end_game(0); // Game over Player lost
     }
 
@@ -92,13 +115,17 @@ void handle_wire_pull(int wire_index) {
 
 void end_game(int end_of_game){
 	game_over = 1;
-	stop_timer();
 	if (end_of_game == 1){
 		set_led_state(0b11111111);
+		stop_timer();
+
 
 	}
 	else {
 		set_led_state(0b10101010);
+                update_display(0);
+		stop_timer();
+		buzzer_on();
 	}
 
 }
